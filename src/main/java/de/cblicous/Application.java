1832b8d140aaaa3a98aad6962ca97119681b9e3a
Christian Belka <Christian.Belka@mgm-tp.com>
2017-09-14 13:39:39 +0200

    furhter develop

--- src/main/java/de/cblicous/Application.java
+++ src/main/java/de/cblicous/Application.java
@@ -2,6 +2,8 @@ package de.cblicous;
 
 import java.util.Arrays;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,17 +12,15 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan
 public class Application {
-    
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-        
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-    }
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		Logger logger = LogManager.getLogger(Application.class);
+		logger.info("Let's inspect the beans provided by Spring Boot and used by GraphQL:");
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		Arrays.stream(beanNames).forEach((springClass) -> logger.info(springClass));
+
+	}
 
 }
